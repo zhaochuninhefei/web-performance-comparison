ace3a8534f3f5f4e0e2001e5634152ab5d4788a3
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-12-16 16:05:45 +0800

    添加zclog配置处理

--- web-pm-go/initialize/initialize_test.go
+++ web-pm-go/initialize/initialize_test.go
@@ -3,13 +3,14 @@ package initialize
 import (
 	"fmt"
 	"github.com/zhaochuninhefei/web-performance-comparison/web-pm-go/config"
+	"github.com/zhaochuninhefei/web-performance-comparison/web-pm-go/global"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"testing"
 )
 
 func TestInitAppConfig(t *testing.T) {
-	bytesYaml, _ := ioutil.ReadFile("../test/appConfigTemp.yaml")
+	bytesYaml, _ := ioutil.ReadFile("/home/zhaochun/work/sources/github.com/zhaochuninhefei/web-performance-comparison/web-pm-go/config/app.yaml")
 	tmpConfig := &config.AppicationConfig{}
 	err := yaml.Unmarshal(bytesYaml, &tmpConfig)
 	if err != nil {
@@ -17,10 +18,12 @@ func TestInitAppConfig(t *testing.T) {
 		return
 	}
 	fmt.Printf("LogFileDir: %s\n", tmpConfig.Log.LogFileDir)
+	tmpConfigYaml, _ := yaml.Marshal(tmpConfig)
+	fmt.Printf("应用配置: \n%s\n", tmpConfigYaml)
 
-	//InitAppConfig("../test/appConfigTemp.yaml")
-	//fmt.Printf("LogFileDir: %s\n", global.AppConfig.Log.LogFileDir)
-	//yamlAppConfig, _ := yaml.Marshal(global.AppConfig)
-	////zclog.Debugf("当前配置: \n%s", string(yamlAppConfig))
-	//fmt.Printf("应用配置: \n%s\n", yamlAppConfig)
+	InitAppConfig("/home/zhaochun/work/sources/github.com/zhaochuninhefei/web-performance-comparison/web-pm-go/config/app.yaml")
+	fmt.Printf("LogFileDir: %s\n", global.AppConfig.Log.LogFileDir)
+	yamlAppConfig, _ := yaml.Marshal(global.AppConfig)
+	//zclog.Debugf("当前配置: \n%s", string(yamlAppConfig))
+	fmt.Printf("应用配置: \n%s\n", yamlAppConfig)
 }
