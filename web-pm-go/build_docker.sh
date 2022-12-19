f9e4d383fdda9f9b26c5ac8d150f3c26eb820ad7
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-12-19 14:20:12 +0800

    修改docker编译脚本

--- web-pm-go/build_docker.sh
+++ web-pm-go/build_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-#CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o web-pm-go .
+set -e
 
-docker build -t web-pm-go:0.0.1 .
+APP_NAME=web-pm-go
+APP_VERSION=0.0.1
+
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ${APP_NAME} .
+
+docker build -t ${APP_NAME}:${APP_VERSION} .
