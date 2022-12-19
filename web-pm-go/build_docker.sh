59cff34246056899a4bbead12f07044eaf8e2df2
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-12-19 14:51:03 +0800

    修改dockerfile及其编译脚本

--- web-pm-go/build_docker.sh
+++ web-pm-go/build_docker.sh
@@ -2,9 +2,44 @@
 
 set -e
 
-APP_NAME=web-pm-go
-APP_VERSION=0.0.1
+DEFAULT_APP_NAME=web-pm-go
+DEFAULT_APP_VERSION=0.0.1
 
+echo "您现在位于: "
+pwd
+echo
+read -r -p "请输入应用名(默认 ${DEFAULT_APP_NAME} ):" APP_NAME
+if [ "${APP_NAME}" == "" ]
+then
+  APP_NAME=${DEFAULT_APP_NAME}
+fi
+echo
+read -r -p "请输入应用版本(默认 ${DEFAULT_APP_VERSION} ):" APP_VERSION
+if [ "${APP_VERSION}" == "" ]
+then
+  APP_VERSION=${DEFAULT_APP_VERSION}
+fi
+echo
+echo "您输入的应用名与应用版本是: ${APP_NAME}:${APP_VERSION}"
+echo
+read -r -p "请确定是否无误，是否继续?(y/n) " goon
+if [ ! "$goon" == "y" ]
+then
+ exit 1
+fi
+
+echo
+echo "===== 开始编译二进制可执行文件: ${APP_NAME} ====="
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ${APP_NAME} .
+echo "编译二进制文件: "
+ls -l ${APP_NAME}
 
+echo
+echo "===== 开始编译docker镜像: ${APP_NAME}:${APP_VERSION} ====="
 docker build -t ${APP_NAME}:${APP_VERSION} .
+echo
+echo "docker编译结果:"
+docker images | grep ${APP_NAME}
+echo
+echo "编译结束."
+echo
