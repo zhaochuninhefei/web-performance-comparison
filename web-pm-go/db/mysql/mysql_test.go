54ac24ad51c5edf5b4b7116f9e67ccab37a1687d
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-12-22 11:30:28 +0800

    添加TestCreatTable

--- web-pm-go/db/mysql/mysql_test.go
+++ web-pm-go/db/mysql/mysql_test.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"gitee.com/zhaochuninhefei/zcgolog/zclog"
+	"github.com/zhaochuninhefei/web-performance-comparison/web-pm-go/db/model"
 	"testing"
 )
 
@@ -22,4 +23,18 @@ func TestConnectMysqlByDefault(t *testing.T) {
 		return
 	}
 	zclog.Infof("select now(): %s", nowStr)
+}
+
+func TestCreatTable(t *testing.T) {
+	mysqlClient, err := ConnectMysqlByDefault("172.17.13.44", "3306", "root", "overseas", "db_web_pm")
+	if err != nil {
+		zclog.Errorln(err)
+		return
+	}
+	err = mysqlClient.Set("gorm:table_options", "ENGINE=InnoDB COMMENT='帐号表'").AutoMigrate(&model.Account{})
+	if err != nil {
+		zclog.Errorln(err)
+		return
+	}
+	zclog.Info("TestCreatTable over.")
 }
