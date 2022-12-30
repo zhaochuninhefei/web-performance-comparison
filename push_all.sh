a3016164dcc5659473398a0a2fb5acc32cfd4af0
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-12-30 09:10:33 +0800

    修改push脚本

--- push_all.sh
+++ push_all.sh
@@ -4,9 +4,9 @@ echo "----- 查看所有远程仓库:"
 git remote -v
 
 echo "----- 推送到github远程仓库:"
-git push origin -all
+git push origin --all
 
 echo "----- 推送到gitee远程仓库:"
-git push gitee -all
+git push gitee --all
 
 echo "----- 推送结束"
