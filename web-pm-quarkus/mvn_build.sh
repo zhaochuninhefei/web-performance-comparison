1b36a8d6cdc434e7317a71a90ac2d22645806542
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-01-04 14:22:16 +0800

    修改数据库连接池配置

--- web-pm-quarkus/mvn_build.sh
+++ web-pm-quarkus/mvn_build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+set -e
+
+echo
+read -r -p "请选择编译方式: 1:uber-jar;2:native (默认 2:native ):" build_mode
+if [ ! "${build_mode}" == "1" ]
+then
+  build_mode=native
+else
+  build_mode=uber-jar
+fi
+
 export JAVA_HOME=/usr/java/jdk-17.0.3+7
 mvn -version
 
-mvn clean install package -Dquarkus.package.type=uber-jar
+mvn clean install package -Dquarkus.package.type=${build_mode}
--- web-pm-quarkus/start_local.sh
+++ web-pm-quarkus/start_local.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-export JAVA_HOME=/usr/java/jdk-17.0.3+7
-
-${JAVA_HOME}/bin/java -jar target/web-pm-quarkus-0.0.1-runner.jar -Dsmallrye.config.locations=./external.yml
+./target/web-pm-quarkus-0.0.1-runner -Dsmallrye.config.locations=./external.yml
