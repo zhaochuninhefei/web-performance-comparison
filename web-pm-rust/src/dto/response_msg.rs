6e7edce9b44ff36defef5d1d1152fc08937c26f2
尹蛟龙 <yinjl26@dimpt.com>
2023-01-06 17:44:16 +0800

    按官方推荐修改了字段名(不用驼峰而是用下划线分隔)

--- web-pm-rust/src/dto/response_msg.rs
+++ web-pm-rust/src/dto/response_msg.rs
@@ -3,6 +3,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase", default)]
 pub struct ResponseMsg {
-    pub resCd: String,
-    pub resMsg: String,
+    pub res_cd: String,
+    pub res_msg: String,
 }
