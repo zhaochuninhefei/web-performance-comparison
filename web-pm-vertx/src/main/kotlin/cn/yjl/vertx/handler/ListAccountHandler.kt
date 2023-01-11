6b2550b46ec052bf96497972757d4c0c0b1c2f32
尹蛟龙 <yinjl26@dimpt.com>
2023-01-11 17:41:02 +0800

    换一个做法

--- web-pm-vertx/src/main/kotlin/cn/yjl/vertx/handler/ListAccountHandler.kt
+++ web-pm-vertx/src/main/kotlin/cn/yjl/vertx/handler/ListAccountHandler.kt
@@ -2,17 +2,20 @@ package cn.yjl.vertx.handler
 
 import cn.yjl.vertx.entity.Accounts
 import cn.yjl.vertx.util.end
-import cn.yjl.vertx.util.toEntity
+import cn.yjl.vertx.util.toJsonArray
 import io.vertx.ext.web.RoutingContext
 import io.vertx.kotlin.coroutines.await
 import io.vertx.sqlclient.SqlClient
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 class ListAccountHandler(private val mysqlClient: SqlClient): AbstractHandler {
 
     override suspend fun handle(context: RoutingContext) {
         val result = mysqlClient.preparedQuery("select * from accounts").execute().await()
-        val jsonResult = result.toList().map { it.toEntity<Accounts>() }
-        context.end(jsonResult).await()
+        val jsonString = result.toList().map { it.toJson() }.toJsonArray().toString()
+        val entityList = Json.decodeFromString<List<Accounts>>(jsonString)
+        context.end(entityList).await()
     }
 
 }
