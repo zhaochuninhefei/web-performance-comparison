0182ab82bb2ac9b9e02a63012ad79c1e9c9763e3
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-06-19 09:21:35 +0800

    添加webflux代码

---
+++ web-pm-webflux/src/main/java/com/zhaochuninhefei/webpmwebflux/WebPmWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.zhaochuninhefei.webpmwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPmWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPmWebfluxApplication.class, args);
+	}
+
+}
