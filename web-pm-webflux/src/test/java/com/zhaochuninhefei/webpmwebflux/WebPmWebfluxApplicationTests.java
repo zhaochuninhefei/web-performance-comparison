5f77aa5c965e46cf520e0de595dd7fef54a37b0f
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-06-19 14:07:07 +0800

    开发webflux代码

---
+++ web-pm-webflux/src/test/java/com/zhaochuninhefei/webpmwebflux/WebPmWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaochuninhefei.webpmwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPmWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
