026dad3ff1f8dbe231996da70b9dad0ee84d6c9c
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-06-20 10:13:42 +0800

    开发webflux代码

--- web-pm-webflux/src/main/java/com/zhaochuninhefei/webpmwebflux/db/repo/AccountsRepository.java
+++ web-pm-webflux/src/main/java/com/zhaochuninhefei/webpmwebflux/db/repo/AccountsRepository.java
@@ -4,9 +4,4 @@ import com.zhaochuninhefei.webpmwebflux.db.po.Accounts;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 public interface AccountsRepository extends R2dbcRepository<Accounts, Long> {
-//    @NotNull
-//    Flux<Accounts> findAll();
-//
-//    @NotNull
-//    Mono<Accounts> findById(@NotNull Long id);
 }
