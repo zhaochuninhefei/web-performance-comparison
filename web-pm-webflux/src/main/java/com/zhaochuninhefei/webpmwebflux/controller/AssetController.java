ea088512a5618781491ee33266d90b0c7a93970d
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-08-19 11:56:03 +0800

    开发webflux代码

--- web-pm-webflux/src/main/java/com/zhaochuninhefei/webpmwebflux/controller/AssetController.java
+++ web-pm-webflux/src/main/java/com/zhaochuninhefei/webpmwebflux/controller/AssetController.java
@@ -1,7 +1,12 @@
 package com.zhaochuninhefei.webpmwebflux.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.zhaochuninhefei.webpmwebflux.dto.Asset;
+import com.zhaochuninhefei.webpmwebflux.dto.ResponseMsg;
+import com.zhaochuninhefei.webpmwebflux.service.AssetService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * @author zhaochun
@@ -9,4 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/asset")
 public class AssetController {
+
+    private final AssetService assetService;
+
+    public AssetController(AssetService assetService) {
+        this.assetService = assetService;
+    }
+
+    @GetMapping("/list")
+    public Mono<List<Asset>> queryAllAssets() {
+        return assetService.queryAssetList();
+    }
+
+    @GetMapping("/query")
+    public Mono<Asset> queryAssetById(@RequestParam String id) {
+        return assetService.queryFromMap(id);
+    }
+
+    @PostMapping("/modify")
+    public Mono<ResponseMsg<Asset>> modifyAsset(@RequestBody Asset asset) {
+        return assetService.modifyAssetDto(asset);
+    }
 }
