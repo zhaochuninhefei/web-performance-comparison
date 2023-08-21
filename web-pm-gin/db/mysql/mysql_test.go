0e363a8ac207bd9ffa768e68dc27365341a58192
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-08-21 13:41:21 +0800

    添加mysql5.7相关脚本

--- web-pm-gin/db/mysql/mysql_test.go
+++ web-pm-gin/db/mysql/mysql_test.go
@@ -101,3 +101,25 @@ func TestInsert1000(t *testing.T) {
 	result := mysqlClient.Create(&acts)
 	zclog.Infof("Insert 件数: %d", result.RowsAffected)
 }
+
+func TestInsert1000_mysql5(t *testing.T) {
+	mysqlClient, err := ConnectMysqlByDefault("localhost", "3306", "zhaochun1", "zhaochun@GITHUB", "db_web_pm")
+	if err != nil {
+		zclog.Errorln(err)
+		return
+	}
+	var acts [1000]model.Account
+	for i := 0; i < 1000; i++ {
+		act := model.Account{
+			ActName:         "libai",
+			ActPwd:          "libai@DATANG",
+			ActNickName:     "诗仙太白",
+			ActIntroduction: "李白，唐朝诗人，字太白，号青莲居士，世称诗仙。",
+			ActStatus:       0,
+			ActRegisterDate: time.Now(),
+		}
+		acts[i] = act
+	}
+	result := mysqlClient.Create(&acts)
+	zclog.Infof("Insert 件数: %d", result.RowsAffected)
+}
