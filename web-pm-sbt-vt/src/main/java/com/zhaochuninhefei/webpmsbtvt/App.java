d2af780a9520b58e34318a8a4ad07f7c1c9b3877
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-01-19 14:17:33 +0800

    添加代码

--- web-pm-sbt-vt/src/main/java/com/zhaochuninhefei/webpmsbtvt/App.java
+++ web-pm-sbt-vt/src/main/java/com/zhaochuninhefei/webpmsbtvt/App.java
@@ -1,7 +1,16 @@
 package com.zhaochuninhefei.webpmsbtvt;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author zhaochun
  */
+@SpringBootApplication
+@MapperScan("com.zhaochuninhefei.webpmsbtvt.db.dao")
 public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
