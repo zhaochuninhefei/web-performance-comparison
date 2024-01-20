092da6cf5fb9a55537fdc76efc3c912f4a288e8f
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-01-20 10:50:30 +0800

    添加helidon-se的jemeter测试用例

--- web-pm-helidon-se/start_local.sh
+++ web-pm-helidon-se/start_local.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export JAVA_HOME=/usr/java/jdk-21.0.1+12
+#export JAVA_OPS="-Djdk.tracePinnedThreads=full"
 
-${JAVA_HOME}/bin/java -jar ./target/web-pm-helidon-se.jar
+${JAVA_HOME}/bin/java ${JAVA_OPS} -jar ./target/web-pm-helidon-se.jar
