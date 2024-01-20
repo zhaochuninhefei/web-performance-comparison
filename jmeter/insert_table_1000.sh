99e35c28ee7eaa745c910dcfb1993d3352a0a4b8
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-01-20 11:27:47 +0800

    修改db数据准备脚本

--- jmeter/insert_table_1000.sh
+++ jmeter/insert_table_1000.sh
@@ -4,6 +4,6 @@ curdir=$(pwd)
 work_dir=~/work/sources/github.com/zhaochuninhefei/web-performance-comparison/web-pm-gin/db/mysql
 
 cd "${work_dir}" || exit
-go test -v -run TestInsert1000
+go test -v -run "^TestInsert1000$"
 
 cd "${curdir}" || exit
