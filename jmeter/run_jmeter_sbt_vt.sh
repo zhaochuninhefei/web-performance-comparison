850ee6138fd27caa68d8b51fc45d677b06175024
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-01-22 17:29:12 +0800

    提交sbt-vt mariadb的测试结果

--- jmeter/run_jmeter_sbt_vt.sh
+++ jmeter/run_jmeter_sbt_vt.sh
@@ -8,39 +8,39 @@ prefix_all=500_3m
 test_cnt=3
 
 
-# 1. 接口/account/add测试开始
-echo
-echo "接口/account/add测试开始..."
-cd ./${test_target}-account_add
+# # 1. 接口/account/add测试开始
+# echo
+# echo "接口/account/add测试开始..."
+# cd ./${test_target}-account_add
 
-for((i=1;i<=test_cnt;i++)); do
-    echo "${i}"
-    echo
-    echo "接口/account/add测试 第${i}轮..."
-    # 第i轮
-    # 清空测试表
-    echo
-    echo "清空测试表"
-    ../clear_table.sh
-    echo
-    echo "等待5秒"
-    sleep 5s
-    # 执行测试计划
-    echo
-    echo "执行测试计划"
-    ./run_jmeter.sh ${test_target}-account_add.jmx ${prefix_all}_${i}
-    echo
-    if [ ! "${i}" == "${test_cnt}" ]
-    then
-        echo
-        echo "等待1分钟"
-        sleep 60s
-    fi
-done
+# for((i=1;i<=test_cnt;i++)); do
+#     echo "${i}"
+#     echo
+#     echo "接口/account/add测试 第${i}轮..."
+#     # 第i轮
+#     # 清空测试表
+#     echo
+#     echo "清空测试表"
+#     ../clear_table_maria.sh
+#     echo
+#     echo "等待5秒"
+#     sleep 5s
+#     # 执行测试计划
+#     echo
+#     echo "执行测试计划"
+#     ./run_jmeter.sh ${test_target}-account_add.jmx ${prefix_all}_${i}
+#     echo
+#     if [ ! "${i}" == "${test_cnt}" ]
+#     then
+#         echo
+#         echo "等待1分钟"
+#         sleep 60s
+#     fi
+# done
 
-cd ../
-echo
-echo "接口/account/add测试结束..."
+# cd ../
+# echo
+# echo "接口/account/add测试结束..."
 
 # 2. 接口/account/list测试开始
 echo
@@ -50,10 +50,10 @@ cd ./${test_target}-account_list
 # 清空测试表
 echo
 echo "清空测试表"
-../clear_table.sh
+../clear_table_maria.sh
 echo
 echo "插入1000条测试数据"
-../insert_table_1000.sh
+../insert_table_1000_maria.sh
 echo
 echo "等待5秒"
 sleep 5s
