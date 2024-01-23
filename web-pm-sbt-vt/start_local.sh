22252e16f10c3ad26108c8fa6026c65d01b80b6d
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-01-23 17:49:07 +0800

    修改mariadb相关配置

--- web-pm-sbt-vt/start_local.sh
+++ web-pm-sbt-vt/start_local.sh
@@ -3,4 +3,4 @@
 export JAVA_HOME=/usr/java/jdk-21.0.1+12
 # export JAVA_OPTS="-Djdk.virtualThreadScheduler.parallelism=200"
 
-${JAVA_HOME}/bin/java ${JAVA_OPTS} -jar ./target/web-pm-sbt-vt-0.0.1.jar
+${JAVA_HOME}/bin/java ${JAVA_OPTS} -jar ./target/web-pm-sbt-vt-*.jar
--- web-pm-springboot/start_local.sh
+++ web-pm-springboot/start_local.sh
@@ -2,4 +2,4 @@
 
 export JAVA_HOME=/usr/java/jdk-17.0.3+7
 
-${JAVA_HOME}/bin/java -jar ./target/web-pm-springboot-0.0.1.jar
+${JAVA_HOME}/bin/java -jar ./target/web-pm-springboot-*.jar
