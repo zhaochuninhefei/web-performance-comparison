294dc55217774b7891c0b97e94c79f638d34a9e1
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-01-26 17:45:50 +0800

    添加mariadb相关测试用例

--- web-pm-gin/db/model/models.go
+++ web-pm-gin/db/model/models.go
@@ -21,3 +21,47 @@ func (act *Account) String() string {
 		"Account{ID:%d, ActName:%s, ActPwd:%s, ActNickName:%s, ActIntroduction:%s, ActStatus:%d, ActRegisterDate:%s}",
 		act.ID, act.ActName, act.ActPwd, act.ActNickName, act.ActIntroduction, act.ActStatus, act.ActRegisterDate)
 }
+
+type Customer struct {
+	gorm.Model
+	CtmName  string `gorm:"type:varchar(100);not null;comment:客户名" json:"ctmName"`
+	CtmEmail string `gorm:"type:varchar(100);not null;comment:客户邮箱" json:"ctmEmail"`
+	CtmPhone string `gorm:"type:varchar(20);not null;comment:客户手机号" json:"ctmPhone"`
+	CtmAddr  string `gorm:"type:varchar(255);not null;comment:客户地址" json:"ctmAddr"`
+	CtmLevel int    `gorm:"type:int;not null;comment:客户级别" json:"ctmLevel"`
+}
+
+func (ctm *Customer) String() string {
+	return fmt.Sprintf(
+		"Customer{ID:%d, CtmName:%s, CtmEmail:%s, CtmPhone:%s, CtmAddr:%s, CtmLevel:%d}",
+		ctm.ID, ctm.CtmName, ctm.CtmEmail, ctm.CtmPhone, ctm.CtmAddr, ctm.CtmLevel)
+}
+
+type Product struct {
+	gorm.Model
+	PrdName  string  `gorm:"type:varchar(100);not null;comment:产品名" json:"prdName"`
+	PrdDesc  string  `gorm:"type:varchar(255);not null;comment:产品描述" json:"prdDesc"`
+	PrdPrice float64 `gorm:"type:decimal(10,2);not null;comment:产品价格" json:"prdPrice"`
+	PrdType  int     `gorm:"type:int;not null;comment:产品类型" json:"prdType"`
+}
+
+func (prd *Product) String() string {
+	return fmt.Sprintf(
+		"Product{ID:%d, PrdName:%s, PrdDesc:%s, PrdPrice:%f, PrdType:%d}",
+		prd.ID, prd.PrdName, prd.PrdDesc, prd.PrdPrice, prd.PrdType)
+}
+
+type Order struct {
+	gorm.Model
+	OrdDate   time.Time `gorm:"type:datetime;size:0;comment:订单日期" json:"ordDate"`
+	OrdStatus uint8     `gorm:"comment:订单状态" json:"ordStatus"`
+	CtmID     uint      `gorm:"type:int;not null;comment:订单客户ID" json:"ctmID"`
+	PrdID     uint      `gorm:"type:int;not null;comment:订单产品ID" json:"prdID"`
+	PrdNum    int       `gorm:"type:int;not null;comment:订单产品数量" json:"prdNum"`
+}
+
+func (ord *Order) String() string {
+	return fmt.Sprintf(
+		"Order{ID:%d, OrdDate:%s, OrdStatus:%d, CtmID:%d, PrdID:%d, PrdNum:%d}",
+		ord.ID, ord.OrdDate, ord.OrdStatus, ord.CtmID, ord.PrdID, ord.PrdNum)
+}
--- web-pm-gin/db/mysql/mysql_test.go
+++ web-pm-gin/db/mysql/mysql_test.go
@@ -1,8 +1,10 @@
 package mysql
 
 import (
+	"fmt"
 	"gitee.com/zhaochuninhefei/zcgolog/zclog"
 	"github.com/zhaochuninhefei/web-performance-comparison/web-pm-gin/db/model"
+	"math/rand"
 	"testing"
 	"time"
 )
@@ -33,6 +35,21 @@ func TestCreatTable(t *testing.T) {
 		return
 	}
 	err = mysqlClient.Set("gorm:table_options", "ENGINE=InnoDB COMMENT='帐号表'").AutoMigrate(&model.Account{})
+	if err != nil {
+		zclog.Errorln(err)
+		return
+	}
+	err = mysqlClient.Set("gorm:table_options", "ENGINE=InnoDB COMMENT='客户表'").AutoMigrate(&model.Customer{})
+	if err != nil {
+		zclog.Errorln(err)
+		return
+	}
+	err = mysqlClient.Set("gorm:table_options", "ENGINE=InnoDB COMMENT='产品表'").AutoMigrate(&model.Product{})
+	if err != nil {
+		zclog.Errorln(err)
+		return
+	}
+	err = mysqlClient.Set("gorm:table_options", "ENGINE=InnoDB COMMENT='订单表'").AutoMigrate(&model.Order{})
 	if err != nil {
 		zclog.Errorln(err)
 		return
@@ -176,4 +193,70 @@ func TestInsert1000_mysql5(t *testing.T) {
 	}
 	result := mysqlClient.Create(&acts)
 	zclog.Infof("Insert 件数: %d", result.RowsAffected)
+}
+
+func TestPrepareCPOIntoMariadb(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+	mysqlClient, err := ConnectMysqlByDefault("localhost", "3308", "zhaochun1", "zhaochun@GITHUB", "db_web_pm")
+	if err != nil {
+		zclog.Errorln(err)
+		return
+	}
+	mysqlClient.Exec("TRUNCATE TABLE customers")
+	// 插入10万个客户数据到客户表 customers
+	for i := 0; i < 100; i++ {
+		var customers [1000]model.Customer
+		for j := 0; j < 1000; j++ {
+			ctmNum := i*1000 + j + 1
+			customer := model.Customer{
+				// 客户名: "客户000001" ~ "客户100000", 番号补0满6位
+				CtmName:  fmt.Sprintf("客户%06d", ctmNum),
+				CtmEmail: fmt.Sprintf("客户%06d@github.com", ctmNum),
+				CtmPhone: fmt.Sprintf("13899%06d", ctmNum),
+				CtmAddr:  fmt.Sprintf("地址%06d", ctmNum),
+				CtmLevel: ctmNum % 5,
+			}
+			customers[j] = customer
+		}
+		result := mysqlClient.Create(&customers)
+		zclog.Infof("Insert 件数: %d", result.RowsAffected)
+	}
+
+	mysqlClient.Exec("TRUNCATE TABLE products")
+	// 插入1万个产品数据到产品表 products
+	for i := 0; i < 10; i++ {
+		var products [1000]model.Product
+		for j := 0; j < 1000; j++ {
+			prdNum := i*1000 + j + 1
+			product := model.Product{
+				// 产品名: "产品000001" ~ "产品100000", 番号补0满6位
+				PrdName:  fmt.Sprintf("产品%06d", prdNum),
+				PrdDesc:  fmt.Sprintf("产品描述%06d", prdNum),
+				PrdPrice: float64(prdNum),
+				PrdType:  prdNum % 5,
+			}
+			products[j] = product
+		}
+		result := mysqlClient.Create(&products)
+		zclog.Infof("Insert 件数: %d", result.RowsAffected)
+	}
+
+	mysqlClient.Exec("TRUNCATE TABLE orders")
+	// 插入100万个订单数据到订单表 orders
+	for i := 0; i < 1000; i++ {
+		var orders [1000]model.Order
+		for j := 0; j < 1000; j++ {
+			order := model.Order{
+				OrdDate:   time.Now(),
+				OrdStatus: uint8(i % 5),
+				// 从 10万个客户ID中随机选取一个
+				CtmID:  uint(rand.Intn(100000) + 1),
+				PrdID:  uint(rand.Intn(10000) + 1),
+				PrdNum: rand.Intn(100) + 1,
+			}
+			orders[j] = order
+		}
+		result := mysqlClient.Create(&orders)
+		zclog.Infof("Insert 件数: %d", result.RowsAffected)
+	}
 }
