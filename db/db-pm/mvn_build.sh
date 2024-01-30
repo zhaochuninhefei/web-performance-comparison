3ffdaf49f75403ead7b95559ea992871476a7a53
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-01-30 14:59:57 +0800

    提交代码

---
+++ db/db-pm/mvn_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/java/jdk-21.0.1+12
+mvn -version
+
+mvn clean install package assembly:single
---
+++ db/db-pm/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/java/jdk-21.0.1+12
+# export JAVA_OPTS="-Djdk.virtualThreadScheduler.parallelism=200"
+
+${JAVA_HOME}/bin/java ${JAVA_OPTS} -cp ./target/db-pm-*-jar-with-dependencies.jar com.zhaochuninhefei.dbpm.DbpmMain 2
