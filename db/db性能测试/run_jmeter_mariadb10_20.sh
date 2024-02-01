5a682b0a099de435f88bff0c71a93a9e79748257
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-02-01 10:03:20 +0800

    添加测试脚本

---
+++ db/db性能测试/run_jmeter_mariadb10_20.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+set -e
+
+work_dir=$(pwd)
+test_target=mariadb10
+prefix_all=20_2m
+test_threads_num=20
+test_cnt=3
+
+
+# 1. insert测试开始
+echo
+echo "insert测试开始..."
+cd ./${test_target}-insert-${test_threads_num}
+
+for((i=1;i<=test_cnt;i++)); do
+    echo "${i}"
+    echo
+    echo "insert测试 第${i}轮..."
+    # 第i轮
+    # 清空测试表
+    echo
+    echo "清空测试表"
+    ../truncateMariaDBTbWarehouse.sh
+    echo
+    echo "等待5秒"
+    sleep 5s
+    # 执行测试计划
+    echo
+    echo "执行测试计划"
+    ./run_jmeter.sh ${test_target}-insert-${test_threads_num}.jmx ${prefix_all}_${i}
+    echo
+    if [ ! "${i}" == "${test_cnt}" ]
+    then
+        echo
+        echo "等待1分钟"
+        sleep 60s
+    fi
+done
+
+cd ../
+echo
+echo "insert测试结束..."
+
+# # 2. joinselect测试开始
+# echo
+# echo "joinselect测试开始..."
+# cd ./${test_target}-joinselect-${test_threads_num}
+
+# # 准备测试数据
+# echo
+# echo "准备测试数据"
+# ../prepareMysqlDataForPT.sh
+# echo
+# echo "等待5秒"
+# sleep 5s
+
+# for((i=1;i<=test_cnt;i++)); do
+#     echo "${i}"
+#     echo
+#     echo "joinselect测试 第${i}轮..."
+#     # 第i轮
+#     # 执行测试计划
+#     echo
+#     echo "执行测试计划"
+#     ./run_jmeter.sh ${test_target}-joinselect-${test_threads_num}.jmx ${prefix_all}_${i}
+#     echo
+#     if [ ! "${i}" == "${test_cnt}" ]
+#     then
+#         echo
+#         echo "等待1分钟"
+#         sleep 60s
+#     fi
+# done
+
+# cd ../
+# echo
+# echo "joinselect测试结束..."
+
+cd "${work_dir}"
