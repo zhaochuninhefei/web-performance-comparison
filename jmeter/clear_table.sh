ea8beb11a7bc2b37beb6fd4d0c37bdda2b1b57ca
zhaochuninhefei <zhaochun.hefei@qq.com>
2025-12-26 10:35:57 +0800

    添加sbt4性能测试脚本

--- jmeter/clear_table.sh
+++ jmeter/clear_table.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curdir=$(pwd)
-work_dir=~/work/sources/github.com/zhaochuninhefei/web-performance-comparison/web-pm-gin/db/mysql
+work_dir=~/sources/github.com/zhaochuninhefei/web-performance-comparison/web-pm-gin/db/mysql
 
 cd "${work_dir}" || exit
 go test -v -run TestTruncateTable
